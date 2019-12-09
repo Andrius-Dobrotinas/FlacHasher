651a2af2f40c082a40456111f96d135e145b7226
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2019-12-09 21:06:30 +0000

    Created the project

---
+++ FlacHasher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlacHasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
