f8762c3e7f6dbd49f093ca77a9a5da3a7d214133
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2019-12-12 21:33:55 +0000

    An experimental implementation of using FLAC decoder to computer the hash of the waveform data

---
+++ FlacHasher/FlacOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FlacHasher
+{
+    public static class FlacOptions
+    {
+        public const string Decode = "-d";
+        public const string WriteToSdtOut = "--stdout";
+    }
+}
--- FlacHasher/Program.cs
+++ FlacHasher/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace FlacHasher
 {
@@ -6,7 +8,41 @@ namespace FlacHasher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var encoderExecutablePath = @"C:\Program Files (x86)\FLAC Frontend\tools\flac.exe";
+            var filePath = @"c:\01 - Hex Me.flac";
+
+            var processSettings = new ProcessStartInfo
+            {
+                FileName = encoderExecutablePath,
+                RedirectStandardOutput = true,
+                RedirectStandardError = false,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                ErrorDialog = false
+            };
+
+            processSettings.ArgumentList.Add(FlacOptions.Decode);
+            processSettings.ArgumentList.Add(FlacOptions.WriteToSdtOut);
+            processSettings.ArgumentList.Add(filePath);
+
+            var encoder = new Process
+            {
+                StartInfo = processSettings
+            };
+
+            encoder.Start();
+
+            using (var waveData = new MemoryStream())
+            {
+                encoder.StandardOutput.BaseStream.CopyTo(waveData);
+
+                using (var algorithm = System.Security.Cryptography.SHA256.Create())
+                {
+                    var hash = algorithm.ComputeHash(waveData);
+
+                    Console.WriteLine(BitConverter.ToString(hash));
+                }
+            }
         }
     }
 }
