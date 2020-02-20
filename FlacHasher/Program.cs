3609fa4824b6bff887bddb237cdede4416b2121e
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-02-20 22:13:36 +0000

    Added checks for files' presence. Made application return status codes.

--- FlacHasher/Program.cs
+++ FlacHasher/Program.cs
@@ -1,20 +1,34 @@
 ﻿using FlacHasher.Crypto;
 using System;
+using System.IO;
 
 namespace FlacHasher
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var encoderExecutablePath = @"C:\Program Files (x86)\FLAC Frontend\tools\flac.exe";
-            var sourceFilePath = @"c:\01 - Hex Me.flac";
+            var encoderExecutable = new FileInfo(@"C:\Program Files (x86)\FLAC Frontend\tools\flac.exe");
+            if (!encoderExecutable.Exists)
+            {
+                Console.WriteLine("The encoder executable file doesn't exist");
+                return -1;
+            }
 
-            var hasher = new Hasher(encoderExecutablePath, new Sha256HashComputer());
+            var sourceFile = new FileInfo(@"c:\01 - Hex Me.flac");
+            if (!sourceFile.Exists)
+            {
+                Console.WriteLine("The source file doesn't exist");
+                return -1;
+            }
 
-            byte[] hash = hasher.ComputerHash(sourceFilePath);
+            var hasher = new Hasher(encoderExecutable.FullName, new Sha256HashComputer());
+
+            byte[] hash = hasher.ComputerHash(sourceFile.FullName);
 
             Console.WriteLine(BitConverter.ToString(hash));
+
+            return 0;
         }
     }
 }
