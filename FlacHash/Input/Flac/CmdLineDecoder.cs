a95b9fe9f28fea902797588a0401ad8347f0be52
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-04-02 21:04:28 +0100

    Dealing with a decoder failure

--- FlacHash/Input/Flac/CmdLineDecoder.cs
+++ FlacHash/Input/Flac/CmdLineDecoder.cs
@@ -19,6 +19,34 @@ namespace Andy.FlacHash.Input.Flac
 
         public Stream Read(FileInfo sourceFile)
         {
+            using (var process = GetProcess(sourceFile, decoderExecutableFile))
+            {
+                process.Start();
+
+                var hashStream = new MemoryStream();
+                process.StandardOutput.BaseStream.CopyTo(hashStream);
+
+                if (!process.HasExited)
+                {
+                    // Should exit right away, this is just in case
+                    process.WaitForExit(1000);
+                    process.Kill(true);
+                }
+
+                if (process.ExitCode != 0)
+                {
+                    using (var reader = new StreamReader(process.StandardError.BaseStream))
+                    {
+                        throw new Exception($"Somethn' went wron': {reader.ReadToEnd()}"); // TODO: exception type
+                    }                    
+                }
+
+                return hashStream;
+            }
+        }
+
+        private static Process GetProcess(FileInfo sourceFile, FileInfo decoderExecutableFile)
+        {
             if (sourceFile == null) throw new ArgumentNullException(nameof(sourceFile));
 
             var processSettings = GetProcessSettings(decoderExecutableFile);
@@ -28,14 +56,10 @@ namespace Andy.FlacHash.Input.Flac
 
             processSettings.ArgumentList.Add(sourceFile.FullName);
 
-            var process = new Process
+            return new Process
             {
                 StartInfo = processSettings
             };
-
-            process.Start();
-
-            return process.StandardOutput.BaseStream;
         }
 
         private static ProcessStartInfo GetProcessSettings(FileInfo encoderExecutablePath)
@@ -44,7 +68,7 @@ namespace Andy.FlacHash.Input.Flac
             {
                 FileName = encoderExecutablePath.FullName,
                 RedirectStandardOutput = true,
-                RedirectStandardError = false,
+                RedirectStandardError = true,
                 UseShellExecute = false,
                 CreateNoWindow = true,
                 ErrorDialog = false
