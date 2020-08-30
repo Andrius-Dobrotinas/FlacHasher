eaca0b4aea05dd5d92cb40f7727dbf410eacc192
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-08-30 19:28:30 +0100

    The dividing number in Progress bar increased in order to accommodate it for real-life FLAC file sizes

--- FlacHasher.Win/UI/Common/FileSizeProgressBarAdapter.cs
+++ FlacHasher.Win/UI/Common/FileSizeProgressBarAdapter.cs
@@ -6,6 +6,11 @@ namespace Andy.FlacHash.Win.UI
 {
     public class FileSizeProgressBarAdapter
     {
+        /* todo: think about what to do with this. technically, this should be a value
+         * that guarantees that the result doesn't exceed int size... but then the real-world
+         * values (file sizes) could be too small? think about this at some point */
+        private const int megabyteDivider = 1048576; //1024x1024
+
         private readonly ProgressBar progressBar;
 
         public FileSizeProgressBarAdapter(ProgressBar progressBar)
@@ -15,14 +20,14 @@ namespace Andy.FlacHash.Win.UI
 
         public void Increment(long bytesRead)
         {
-            long progress = bytesRead / 1024;
+            long progress = bytesRead / megabyteDivider;
 
             progressBar.Increment((int)progress);
         }
 
         public void SetMaxValue(long value)
         {
-            long maximum = value / 1024;
+            long maximum = value / megabyteDivider;
             progressBar.Maximum = (int)maximum;
         }
     }
