e35c3414700b7d0d531b9b0f5ab52912cd2ba402
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2020-09-03 20:24:26 +0100

    Refactoring: function broken down a bit for readability

--- FlacHash.IO/ExternalProcess/ProcessRunner.cs
+++ FlacHash.IO/ExternalProcess/ProcessRunner.cs
@@ -36,9 +36,9 @@ namespace Andy.FlacHash.ExternalProcess
         public Stream RunAndReadOutput(
             FileInfo fileToRun,
             IEnumerable<string> arguments,
-            Stream input)
+            Stream inputData)
         {
-            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (inputData == null) throw new ArgumentNullException(nameof(inputData));
 
             var processSettings = ProcessStartInfoFactory.GetStandardProcessSettings(fileToRun, arguments);
             processSettings.RedirectStandardInput = true;
@@ -49,21 +49,26 @@ namespace Andy.FlacHash.ExternalProcess
 
                 System.Threading.Tasks.Task.Delay(100).Wait(); //throws a "Pipe ended" error when trying to write to std right away. Waiting a bit before writing seems to solve the problem, but this could be problematic if the system is slower...
 
-                //the writing operation gets blocked until someone reads the output - when both in and out are redirected... at least with Flac
+                //the writing operation gets blocked until someone reads the output - when both in and out are redirected.
                 var outputReadTask = System.Threading.Tasks.Task.Run<MemoryStream>(() => GetStandardOutput(process));
 
-                var stdin = process.StandardInput.BaseStream;
-                input.CopyTo(stdin);
-
-                //it looks like either the stream has to be closed, or an "end of file" char (-1 in int language) must be written to the stream
-                stdin.Close();
+                WriteToStdIn(process, inputData);
 
                 var outputStream = GetTaskResult(outputReadTask);
-                
+
                 ProcessExitCode(process);
 
                 return outputStream;
             }
+        }
+
+        private static void WriteToStdIn(Process process, Stream inputData)
+        {
+            var stdin = process.StandardInput.BaseStream;
+            inputData.CopyTo(stdin);
+
+            //it looks like either the stream has to be closed, or an "end of file" char (-1 in int language) must be written to the stream
+            stdin.Close();
         }
 
         private static Stream GetTaskResult(System.Threading.Tasks.Task<MemoryStream> outputReadTask)
@@ -80,6 +85,8 @@ namespace Andy.FlacHash.ExternalProcess
 
         private static MemoryStream GetStandardOutput(Process process)
         {
+            //std-out gets disposed of when the process is disposed of
+
             var processOutput = new MemoryStream();
             process.StandardOutput.BaseStream.CopyTo(processOutput);
 
