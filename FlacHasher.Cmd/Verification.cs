5a6724dc29703f2c7026ed84177d1de573b50dbe
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2024-07-01 17:35:25 +0100

    Micro-optimisation

--- FlacHasher.Cmd/Verification.cs
+++ FlacHasher.Cmd/Verification.cs
@@ -49,11 +49,11 @@ namespace Andy.FlacHash.Cmd
         private static IList<KeyValuePair<FileInfo, HashMatch>> VerifyHashes(IList<FileInfo> files, FileHashMap expectedHashes, HashVerifier hashVerifier, IMultipleFileHasher hasher, CancellationToken cancellation)
         {
             var fileHashes = HashEntryMatching.MatchFilesToHashes(expectedHashes, files);
-            var results = new List<KeyValuePair<FileInfo, HashMatch>>();
+            var results = new List<KeyValuePair<FileInfo, HashMatch>>(fileHashes.Count);
 
             foreach (FileHashResult calcResult in hasher.ComputeHashes(fileHashes.Keys, cancellation))
-                {
-                    cancellation.ThrowIfCancellationRequested();
+            {
+                cancellation.ThrowIfCancellationRequested();
 
                 if (calcResult.Exception == null)
                 {
