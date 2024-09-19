88ddfe7a9e274b618b93fbc78f9226d9b38fc2a6
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2024-09-19 20:30:47 +0100

    Error message formatting

--- Andy.Cmd/Parameter/Exceptions/ParameterDependencyUnmetException.cs
+++ Andy.Cmd/Parameter/Exceptions/ParameterDependencyUnmetException.cs
@@ -4,7 +4,8 @@ namespace Andy.Cmd.Parameter
 {
     public class ParameterDependencyUnmetException : ParameterMissingException
     {
-        public ParameterDependencyUnmetException(string paramName, string masterParamName) : base($"A mandatory parameter (required by {masterParamName}) was not supplied", paramName)
+        public ParameterDependencyUnmetException(string paramName, string masterParamName) 
+            : base($"A mandatory parameter required by {masterParamName} was not supplied", paramName)
         {
         }
     }
