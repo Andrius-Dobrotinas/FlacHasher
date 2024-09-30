a8fae7c1fff9b37ce4ea4ff1d52b937b5ed48942
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2024-09-30 17:37:17 +0100

    File size reduction, better discoverability

--- FlacHasher.Cmd/Program.cs
+++ FlacHasher.Cmd/Program.cs
@@ -159,22 +159,5 @@ namespace Andy.FlacHash.Cmd
             }
             Console.Error.WriteLine(text);
         }
-
-        public class InitialParams
-        {
-            /// <summary>
-            /// When non-null, specifies a settings profile to use.
-            /// Overrides one configured in the settings file.
-            /// Empty value results in overriding a pre-configured Profile value to use the default one.
-            /// </summary>
-            [Parameter(ParameterNames.Profile)]
-            [Optional]
-            [AllowEmpty]
-            public string Profile { get; set; }
-
-            [CmdLineParameter(ParameterNames.ModeVerify)]
-            [Optional]
-            public bool IsVerification { get; set; }
-        }
     }
 }
