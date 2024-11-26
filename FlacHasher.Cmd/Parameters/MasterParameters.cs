192226b0802d2e280f9891f155a9eaee6c4b174b
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2024-11-26 19:52:48 +0000

    [Cmd-line, Parameters] Help: parameter descriptions

--- FlacHasher.Cmd/Parameters/MasterParameters.cs
+++ FlacHasher.Cmd/Parameters/MasterParameters.cs
@@ -8,6 +8,7 @@ namespace Andy.FlacHash.Application.Cmd
     {
         [CmdLineParameter(CmdlineParameterNames.Decoder, Order = 0)]
         [IniEntry("Decoder", Order = 1)]
+        [ParameterDescription("Path to the Audio decoder executable file; if just a file name is given (without a path), it gets looked up in paths configured in PATH environment variable. If not specified, the value is assumed to be \"flac.exe\"")]
         public string DecoderExe { get; set; }
 
         /// <summary>
@@ -17,6 +18,7 @@ namespace Andy.FlacHash.Application.Cmd
         /// </summary>
         [IniEntry(nameof(DecoderParameters))]
         [Optional]
+        [ParameterDescription("An array of parameters to the Audio decoder exactly the way they are supposed to appear (with dashes and whatnot), separated by a semi-colon. If not specified, default FLAC parameters are used.")]
         public string[] DecoderParameters { get; set; }
 
         public abstract string InputDirectory { get; set; }
@@ -24,11 +26,14 @@ namespace Andy.FlacHash.Application.Cmd
         [CmdLineParameter(CmdlineParameterNames.FileExtension, Order = 0)]
         [IniEntry(nameof(TargetFileExtension), Order = 1)]
         [RequiredWith(nameof(InputDirectory))]
+        [ParameterDescription("Extension of file type that is accepted by the configured Audio decoder")]
         public string TargetFileExtension { get; set; }
 
         [CmdLineParameter(CmdlineParameterNames.DecoderPrintProgress, Order = 0)]
         [IniEntry(nameof(PrintDecoderProgress), Order = 1)]
+        [RequiredWith(nameof(InputDirectory))]
         [Optional(defaultValue: true)]
+        [ParameterDescription("Tells whether the whole Audio Decoder informational output has to be redirected to the window or kept hidden for less noise")]
         public bool PrintDecoderProgress { get; set; }
     }
 }
