24a9ca338f16567cd9010b93be09103148987976
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2024-11-26 21:47:39 +0000

    [Cmd-line, Parameters] Help: More descriptions

--- FlacHasher/Parameters/ApplicationSettings.cs
+++ FlacHasher/Parameters/ApplicationSettings.cs
@@ -16,6 +16,7 @@ namespace Andy.FlacHash.Application
         [CmdLineParameter(CmdlineParameterNames.ProcessTimeoutSec, Order = 0)]
         [IniEntry(nameof(ProcessTimeoutSec), Order = 1)]
         [Optional(defaultValue: 180)]
+        [ParameterDescription("The maximum amount of time (in seconds) an Audio decoder is allowed to take to decode a file. This is useful for situations where something goes wrong and the decoder stops responding and needs to be murdered")]
         public int ProcessTimeoutSec { get; set; }
 
         [IniEntry(nameof(ProcessStartWaitMs))]
@@ -25,10 +26,12 @@ namespace Andy.FlacHash.Application
         [CmdLineParameter(CmdlineParameterNames.HashAlgorithm, Order = 0)]
         [IniEntry(nameof(HashAlgorithm), Order = 1)]
         [Optional(defaultValue: Defaults.HashAlgorithm)]
+        [ParameterDescription($"Available options: {nameof(Algorithm.MD5)}, {nameof(Algorithm.SHA1)}, {nameof(Algorithm.SHA256)}, {nameof(Algorithm.SHA512)}")]
         public Algorithm HashAlgorithm { get; set; }
 
         [IniEntry(nameof(FileLookupIncludeHidden))]
         [Optional(defaultValue: false)]
+        [ParameterDescription("Whether to include hidden files when scanning a directory")]
         public bool FileLookupIncludeHidden { get; set; }
 
         public const string ProfileKey = "Profile";
