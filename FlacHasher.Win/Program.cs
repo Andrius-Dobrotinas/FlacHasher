c2e5420c2d76c9ef0b03de7a2d13b6bafe1ee02f
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2024-12-03 19:45:12 +0000

    [Winforms] Settings file name has to be different in case both application sit in the same folder

--- FlacHasher.Win/Program.cs
+++ FlacHasher.Win/Program.cs
@@ -12,7 +12,7 @@ namespace Andy.FlacHash.Application.Win
 {
     static class Program
     {
-        public const string settingsFileName = "settings.cfg";
+        public const string settingsFileName = "settings.w.cfg";
 
         [STAThread]
         static void Main()
