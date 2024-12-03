37880c4741cda7fd7bbfb07925c63a5683146c67
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2024-12-03 19:56:42 +0000

    [Cmd-line, Parameters] Error message sorted

--- FlacHasher.Cmd/Program.cs
+++ FlacHasher.Cmd/Program.cs
@@ -71,12 +71,13 @@ namespace Andy.FlacHash.Application.Cmd
             {
                 var property = Metadata.GetParameterMetadata<ParameterAttribute>(e.ParameterProperty.DeclaringType, e.ParameterProperty);
                 var sb = new System.Text.StringBuilder();
+                sb.AppendLine(e.Message);
+                sb.AppendLine();
                 sb.AppendLine($"Provide the configuration for \"{property.DisplayName}\" via:");
                 sb.AppendLine(string.Join(", ", property.Sources.Select(x => $"{x.Name}")));
-
-                WriteUserLine(e.Message);
+                sb.AppendLine();
+                sb.AppendLine(HelpMessage);
                 WriteUserLine(sb.ToString());
-                WriteUserLine(HelpMessage);
                 return (int)ReturnValue.ArgumentNotProvided;
             }
             catch (ParameterGroupException e)
