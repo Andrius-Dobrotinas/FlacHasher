e14d63b84c0258bbeb0480045b413360d0d618cb
Andy Dobrotinas <andrius.dobrotinas@gmail.com>
2024-12-03 19:38:40 +0000

    .

--- FlacHasher.Win/UI/FormX.cs
+++ FlacHasher.Win/UI/FormX.cs
@@ -731,42 +731,7 @@ namespace Andy.FlacHash.Application.Win.UI
             if (keyData == Keys.F1)
             {
                 ResetLog();
-
-                var helpText = Help.GetHelpText();
-                var helpHashfileText = Help.GetHashfileHelpText();
-                var thisAssemply = System.Reflection.Assembly.GetExecutingAssembly();
-                var helpApplicationSpecific = Help.GetTextResource(thisAssemply, "help.txt");
-                var helpText_decoder = Help.GetTextResource(thisAssemply, "help_decoder_config.txt");
-
-                var builder = new System.Text.StringBuilder(helpText);
-                var builderApplicationSpecific = new System.Text.StringBuilder(helpApplicationSpecific);
-
-                builderApplicationSpecific.Replace(Help.Placeholder.HashfileDescription, helpHashfileText);
-                builder.Replace(Help.Placeholder.ApplicationSpecific, builderApplicationSpecific.ToString());
-
-                // Decoder profile params
-                var decoderParamsBuilder = new System.Text.StringBuilder(helpText_decoder);
-                var decoderProfileProperties = typeof(SettingsFile.DecoderProfileTemp).GetProperties().Where(Andy.Cmd.Parameter.Metadata.IsParameter);
-                var cmdlineProperties = typeof(Cmd.MasterParameters).GetProperties().Where(Andy.Cmd.Parameter.Metadata.IsParameter)
-                    .Where(x => decoderProfileProperties.Contains(x, Application.Help.PropertyInfoNameComparer.Instance))
-                    .ToList();
-                var temp = new System.Text.StringBuilder();
-                Help.PrintParameters<Cmd.MasterParameters, IniEntryAttribute>(temp, cmdlineProperties, Array.Empty<System.Reflection.PropertyInfo>());
-                decoderParamsBuilder.Replace("{DECODER_PROFILE_CONFIG}", temp.ToString());
-
-                // Decoder shared stuff
-                var (decoderProperties, opSpecificProperties, miscProperties) = Help.GetPropertiesByParameterPurpose<Settings>();
-                var decoderParamsLine = Help.GetParameterString<Settings, IniEntryAttribute>(decoderProperties, Array.Empty<System.Reflection.PropertyInfo>());
-                decoderParamsBuilder.Replace("{DECODER_CONFIG}", decoderParamsLine);
-
-                builder.Replace(Help.Placeholder.DecoderParams, decoderParamsBuilder.ToString());
-
-                builder.AppendLine("===========================================================");
-                builder.AppendLine($"{Help.Indentation}OTHER SETTINGS");
-                builder.AppendLine();
-                Help.PrintParameters<Settings, IniEntryAttribute>(builder, opSpecificProperties, miscProperties);
-                WriteLine(builder.ToString());
-
+                WriteLine(HelpUtil.GetHelpText().ToString());
                 return true;
             }
 
