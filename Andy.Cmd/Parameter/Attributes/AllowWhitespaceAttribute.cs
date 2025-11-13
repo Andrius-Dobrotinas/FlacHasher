5bbaa2d4cb4a894c1d5e673b4e17277237d26db7
Andrius Dobrotinas <andrius.dobrotinas@gmail.com>
2025-11-13 17:58:38 +0000

    AllowWhitespace attribute: Should have gone with commit dcdb69f86c213ad7ce99a76b779a09d313de2d2f

--- Andy.Cmd/Parameter/Attributes/AllowWhitespaceAttribute.cs
+++ Andy.Cmd/Parameter/Attributes/AllowWhitespaceAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace Andy.Cmd.Parameter
 {
     /// <summary>
-    /// For use with <see cref="string"/> and <see cref="string[]"/> only
+    /// For use with <see cref="string"/> only
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public class AllowWhitespaceAttribute : Attribute
--- FlacHasher.Win/Settings.cs
+++ FlacHasher.Win/Settings.cs
@@ -14,6 +14,7 @@ namespace Andy.FlacHash.Application.Win
 
         [IniEntry(nameof(HashfileEntrySeparator))]
         [Optional(defaultValue: HashFileReader.Default.HashfileEntrySeparator)]
+        [AllowWhitespace]
         [ParameterDescription("A character sequence that separates File-name and Hash-value in a hashfile. Not need when hashfile doesn't contain file names. To specify a Space char via the command line, put the value in quotes (\" \"). Don't use quotes when configuring via the settings file")]
         public string HashfileEntrySeparator { get; set; }
 
