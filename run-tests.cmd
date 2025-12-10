375f9aa68a2198c5a7ddbefeb15153f45568ddd4
Andrius Dobrotinas <andrius.dobrotinas@gmail.com>
2025-12-10 20:35:30 +0000

    Revert "try"
    This reverts commit bd4eb1e80a9f5f6f18ce431e5d6cefba4193756e.

--- run-tests.cmd
+++
@@ -1,3 +0,0 @@
-dotnet build "FlacHasher.sln" --configuration Release /p:EnableWindowsTargeting=true
-
-dotnet test "Andy.Cmd.Tests\\Cmd.Tests.csproj" --configuration Release --no-build --logger "html;LogFileName=c:\TestResults\Cmd.Tests.html"
